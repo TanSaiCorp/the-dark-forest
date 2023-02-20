91085812e4deaac60b1487607c0ee3580746bfa4
tanmoy-bms <53893576+tanmoy-bms@users.noreply.github.com>
2023-02-20 18:57:58 +0530

    Basic Parallax implemented

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    private Transform cameraTransform;
+    public Transform cameraTransform;
+    [SerializeField] private Vector2 relativeMovement;
     private Vector3 previousCameraPosition;
-    // Start is called before the first frame update
+    // public bool lockYMovement = false;
     private void Start()
     {
-        cameraTransform = Camera.main.transform;
+        //cameraTransform = Camera.main.transform;
+        previousCameraPosition = cameraTransform.position;
     }
 
-    // Update is called once per frame
     private void LateUpdate()
     {
         Vector3 deltaMovement = cameraTransform.position - previousCameraPosition;
-        transform.position += deltaMovement;
+        transform.position += new Vector3(deltaMovement.x * relativeMovement.x, deltaMovement.y * relativeMovement.y);
         previousCameraPosition = cameraTransform.position;
+
+        // if (lockYMovement)
+        // {
+        //     transform.position = new Vector2(cameraTransform.position.x * relativeMovement, transform.position.y);
+        // }
+        // else
+        // {
+        //     transform.position = new Vector2(cameraTransform.position.x * relativeMovement, cameraTransform.position.y * relativeMovement);
+        // }
     }
 }
