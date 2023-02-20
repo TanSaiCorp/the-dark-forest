7ec6970d277411de4aea19de145d2835a57ca419
tanmoy-bms <53893576+tanmoy-bms@users.noreply.github.com>
2023-02-20 20:04:33 +0530

    Infinite Scrolling added to background

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -7,26 +7,26 @@ public class ParallaxBackground : MonoBehaviour
     public Transform cameraTransform;
     [SerializeField] private Vector2 relativeMovement;
     private Vector3 previousCameraPosition;
+    private float textureUnitSizeX;
     // public bool lockYMovement = false;
     private void Start()
     {
         //cameraTransform = Camera.main.transform;
         previousCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
     }
 
     private void LateUpdate()
     {
         Vector3 deltaMovement = cameraTransform.position - previousCameraPosition;
-        transform.position += new Vector3(deltaMovement.x * relativeMovement.x, deltaMovement.y * relativeMovement.y);
+        transform.position -= new Vector3(deltaMovement.x * relativeMovement.x, deltaMovement.y * relativeMovement.y);
         previousCameraPosition = cameraTransform.position;
 
-        // if (lockYMovement)
-        // {
-        //     transform.position = new Vector2(cameraTransform.position.x * relativeMovement, transform.position.y);
-        // }
-        // else
-        // {
-        //     transform.position = new Vector2(cameraTransform.position.x * relativeMovement, cameraTransform.position.y * relativeMovement);
-        // }
+        if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX){
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+        }
     }
 }
